a55b45b8b89a65c77dabc428cafdbcc7fabc5668
shruti <shrutiagrawal15@gmail.com>
2017-04-03 11:12:42 -0700

    initial commit

---
+++ Largestpalindromeproduct/Largestpalindromeproduct/main.cpp
@@ -0,0 +1,93 @@
+/*
+ Created by Shruti Agrawal on 4/2/17.
+ Copyright © 2017 Shruti. All rights reserved.
+ 
+ Problem 4
+ A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ Find the largest palindrome made from the product of two 3-digit numbers.
+ 
+ RunTime: O(n^2)
+ */
+
+using namespace std;
+#include <iostream>
+#include <cassert>
+
+/*
+ Description: Method to check whether given number is a Palindrome
+ Input:int
+ Output: bool, True if input is palindrome,false otherwise
+ */
+bool isPalindrome(int num)
+{
+    int digit = 0,reverse = 0, n = 0;
+    n = num;
+    
+    //reverse the given number
+    do
+    {
+        digit = num % 10;
+        reverse = (reverse * 10) + digit;
+        num = num / 10;
+    } while (num != 0);
+    
+    if(n == reverse)
+    {
+        //return true if the reverse of the number is the same as the given number
+        return true;
+    }
+    else
+    {
+        //return false if the reverse of the number is not the same as the given number
+        return false;
+    }
+}
+
+/*
+ Description: Method to find the largest palindrome made from the product of two 3-digit numbers
+ Input: None
+ Output: int,largest palindrome made from the product of two 3-digit numbers
+ */
+int maxPalindrome()
+{
+    int maxPalindrome = -1;
+    
+    for (int i = 100; i< 1000; i++)
+    {
+        for (int j = 100; j< 1000; j++)
+        {
+            
+            int product = i * j;
+            
+            if(isPalindrome(product) && product > maxPalindrome)
+                maxPalindrome = product;
+        }
+    }
+    
+    cout<<"The largest palindrome made from the product of two 3-digit numbers is: "<<maxPalindrome<<endl;
+    return maxPalindrome;
+}
+
+//Method to test the isPalindrome method
+void testIsPalindrome()
+{
+    cout<<endl<<"Entering test:"<<endl;
+    assert(isPalindrome(101) == true);
+    assert(isPalindrome(1010) == false);
+    assert(isPalindrome(10101) == true);
+    assert(isPalindrome(10) == false);
+    assert(isPalindrome(1) == true);
+    assert(isPalindrome(123404321) == true);
+    assert(isPalindrome(906609) == true);
+    assert(isPalindrome(-101) == true);
+    assert(isPalindrome(-1010) == false);
+    assert(isPalindrome(-96969) == true);
+    cout<<"All tests passed for isPalindrome method"<<endl;
+}
+
+//Enter main
+int main(int argc, const char * argv[]) {
+    maxPalindrome();
+    testIsPalindrome();
+    return 0;
+}
---
+++ Multiplesof3and5/Multiplesof3and5/main.cpp
@@ -0,0 +1,54 @@
+/*
+ Created by Shruti Agrawal on 4/2/17.
+ Copyright © 2017 Shruti. All rights reserved.
+ 
+ Problem 1
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ Find the sum of all the multiples of 3 or 5 below 1000.
+ 
+ RunTime: O(n)
+ */
+
+using namespace std;
+#include <iostream>
+#include <cassert>
+
+/*
+ Description: Method to find the sum of multiples of 3 and 5 below 1000
+ Input:int, int
+ Output: int
+ */
+int multiplesOf(int x, int y)
+{
+    int sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        //only add numbers which are multiples of 3 and 5
+        if (i % x == 0 || i % y == 0)
+        {
+            sum += i;
+        }
+    }
+    cout<<"The sum of all multiples of "<<x<< " and "<<y<<" below 1000 is: "<<sum<<endl;
+    return sum;
+}
+
+//Method to test multiplesOf 3 and 5
+void testMultiplesOf()
+{
+    cout<<endl<<"Entering test:"<<endl;
+    assert(multiplesOf(3,5)!= multiplesOf(4,5));
+    assert(multiplesOf(6, 10)< multiplesOf(3, 5));
+    assert(multiplesOf(1, 2) > multiplesOf(3, 5));
+    assert(multiplesOf(3, 5) > 0);
+    cout<<"All tests passed for multiplesOf method"<<endl;
+}
+
+//Entering main
+int main(int argc, const char * argv[]) {
+    int a = 3;
+    int b = 5;
+    multiplesOf(a,b);
+    testMultiplesOf();
+    return 0;
+}
